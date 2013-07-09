e071ea2328fb2f1886a6ed8a75f6335ff7f91ffd
law826 <law826@gmail.com>
2013-07-09 11:11:10 -0400

    Change variable names

--- Neurosynth_SNA.py
+++ Neurosynth_SNA.py
@@ -17,55 +17,72 @@ from pdb import *
 
 
 def SetPaths():
+	"""
+	Set relevant paths for windows, linux, and mac systems.
+	"""
 	if sys.platform == "darwin":
-	    maindir = os.sep.join(['/Volumes', 'KBE.01',  'Analysis', 'Neurosynth', 'ReverseResults'])
-	    outdir = os.sep.join(['/Volumes', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
+		maindir = os.sep.join(['/Volumes', 'huettel', 'KBE.01',  'Analysis', 'Neurosynth', 'ReverseResults'])
+		outdir  = os.sep.join(['/Volumes', 'huettel', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
 	elif sys.platform == "win32":
-	    maindir = os.sep.join(['M:', 'KBE.01', 'Analysis', 'Neurosynth', 'ReverseResults'])
-	    outdir = os.sep.join(['M:', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
+		maindir = os.sep.join(['M:', 'KBE.01', 'Analysis', 'Neurosynth', 'ReverseResults'])
+		outdir  = os.sep.join(['M:', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
 	elif sys.platform == "linux2":
-		import getpass
 		username=getpass.getuser()
 		maindir = os.sep.join(['/home', username, 'experiments', 'KBE.01', 'Analysis', 'Neurosynth', 'ReverseResults'])
-		outdir = os.sep.join(['/home', username, 'experiments', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
-	for files in os.walk(maindir):
+		outdir  = os.sep.join(['/home', username, 'experiments', 'KBE.01', 'Analysis', 'Neurosynth', 'Data'])
+
+	forward_inference_edgelist = os.sep.join([outdir, "forward_inference.txt"])
+	reverse_inference_edgelist = os.sep.join([outdir, "reverse_inference.txt"])
+
+	return maindir, outdir, forward_inference_edgelist, reverse_inference_edgelist
+
+
+def GetFileNamesInDirectory(directory):
+	"""
+	Takes a directory and returns a list of the names of all the files in that directory sorted in alphabetical order. 
+	"""
+	for files in os.walk(directory):
 		for file in files:
 			file_names=file
 	file_names.sort()
 
+	return file_names
 
-
-
-	forward_inference_edgelist = "/Volumes/huettel/KBE.01/Analysis/Neurosynth/Data/forward_inference.txt"
-
-def create_cross_correlation_table():
+def create_cross_correlation_table(maindir, files_names, outdir, outname):
+	"""
+	Takes a directory and list of numpy files and horizontally concatenates them all and saves the output in outdir. 
+	"""
 	for brain in range(0,979):
-		database_brain = N.load(maindir+os.sep+file_names[brain]) # Loading the image
+		database_brain = np.load(maindir+os.sep+file_names[brain]) # Loading the image
 		#database_brain_data = database_brain.get_data() # Accessing numbers from the nibabel object and putting in numpy array.
 		if brain==0:
 			concatenate_data= database_brain
 		else:
-			concatenate_data=N.concatenate((concatenate_data, database_brain), axis=1)
-	outpath=os.sep.join([outdir, 'concatenate_data_matrix'])
-	N.save(outpath, concatenate_data)
+			concatenate_data=np.concatenate((concatenate_data, database_brain), axis=1)
+	outpath=os.sep.join([outdir, outname])
+	np.save(outpath, concatenate_data)
 		
-def create_SNA_table():
+def CreateEdgelist(maindir, file_names, outdir, outname):
+	"""
+	Takes a directory and list of numpy files and vertically concatenates them into an edge list format and saves the output in outdir.
+	"""
 	for i, file_name in enumerate(file_names):
-		database_brain = N.load(maindir+os.sep+file_name) # Loading the data
-		first_column=N.zeros((database_brain.shape[0],1))
-		first_column[:,0]=i
-		second_column=N.arange((database_brain.shape[0]))
+		database_brain      = np.load(maindir+os.sep+file_name) # Loading the data
+		first_column        = np.zeros((database_brain.shape[0],1))
+		first_column[:,0]   = i
+		second_column       = np.arange((database_brain.shape[0]))
 		second_column.shape = (database_brain.shape[0],1)
-		three_col = N.concatenate((first_column, second_column, database_brain), axis=1)
+		three_col           = np.concatenate((first_column, second_column, database_brain), axis=1)
 		if i==0:
-			concatenate_data=three_col
+			concatenate_data = three_col
 		else:
-			concatenate_data=N.concatenate((concatenate_data, three_col), axis=0)
-	outpath=os.sep.join([outdir, 'reverse_inference'])
-	N.save(outpath, concatenate_data)
-	N.savetxt(outpath+'.txt', concatenate_data, fmt='%10.f %10.f %1.3f', delimiter='\t')
+			concatenate_data = np.concatenate((concatenate_data, three_col), axis=0)
+	outpath=os.sep.join([outdir, outname])
+	np.save(outpath, concatenate_data)
+	set_trace()
+	np.savetxt(outpath+'.txt', concatenate_data, fmt='%10.f %10.f %1.3f', delimiter='\t')
 
-def ImportData(file):
+def ImportEdgelist(file):
 	graph = Graph.Read_Edgelist(file)
 	return graph
 
@@ -75,6 +92,19 @@ def VisualizeData():
 def CommonCommands():
 	graph = ImportData(forward_inference_edgelist)
 
+
+"""
+Start of specific user commands.
+"""
+
+def main():
+	pass
+	
 if __name__ == '__main__':
-	SetPaths()
+	maindir, outdir, forward_inference_edgelist, reverse_inference_edgelist = SetPaths()
+	file_names                                                              = GetFileNamesInDirectory(maindir)
+
+	CreateEdgelist(maindir, file_names, outdir, 'reverse_inference2')
+
+
 
